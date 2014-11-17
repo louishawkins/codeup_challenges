b5462972b762a278a5d4968c0ec14550a771b155
Louis Hawkins <louishawkins@gmail.com>
2014-11-17 17:37:24 -0600

    Added heading comment

--- states/states.php
+++ states/states.php
@@ -1,4 +1,24 @@
 <?php
+/*
+ * Solution by Louis Hawkins
+ * 2014
+ *
+ * # States Array
+ *
+ * 1. Create an mulidimensional array out of the states text file.
+ * 2. Create a function that will list the states.
+ * 3. Create a function that will let you search for states by the first letter.
+ * 4. Create a function that will let you search for birds by the first letter.
+ * 5. Create a function that will let you search for capitals by the first letter.
+ *
+ * ## Extra Credit (yet-todo) 
+ *
+ * 1. Create a trivia game. Ask the user a random question like the following:
+ * 2. "What is the capital of ____?"
+ * 3. "What state is Austin in?"
+ * 4. "What is the state bird of California?"
+ */
+
 class StateData {
     public $states_data_file;
     public $all_state_data;
